a4fb526d0594964a18ff2f5daab4342ac3384843
AgustinaDiTommaso <89307742+AgustinaDiTommaso@users.noreply.github.com>
2023-06-26 12:33:12 -0300

    Fix Main.java

--- Main.java
+++ Main.java
@@ -6,73 +6,32 @@ public class Main {
 
 	public static void main(String[] args) {
 		ArrayList<String> paths = new ArrayList<>(Arrays.asList(
-		"../datasets/dataset1.txt",
+			"../datasets/dataset1.txt",
 			"../datasets/dataset2.txt",
 			"../datasets/dataset3.txt"
 		));
 		for (String path : paths) {
 			CSVReader reader= new CSVReader(path);
 			reader.read();
-			//Grafo<Integer> g = reader.getInfo();
 
 			List<Arco<Integer>> tuneles= reader.getTuneles();
-    		List<Integer> estaciones= reader.getEstaciones();
+    			List<Integer> estaciones= reader.getEstaciones();
 
 			ServicioGreedy greedy= new ServicioGreedy(estaciones,tuneles);
 			System.out.println("GREEDY");
 			System.out.println(greedy.dijkstra());
 			System.out.println(greedy.getDistancia()+" KM");
-			System.out.println("Métrica: "+greedy.metrica);
+			System.out.println("Métrica: "+greedy.getMetrica());
 
 			System.out.println(" ");
 
 			ServBacktracking back= new ServBacktracking(estaciones, tuneles);
 			System.out.println("BACKTRACKING");
 			System.out.println(back.backtracking());
-			System.out.println(back.distanciaTotal+" KM");
-			System.out.println("Métrica: "+back.metrica);
+			System.out.println(back.getDistanciaTotal()+" KM");
+			System.out.println("Métrica: "+back.getMetrica());
 			System.out.println("---------");
 		}
-		/* 
-		String path1 = "../datasets/dataset1.txt";
-		CSVReader reader1 = new CSVReader(path1);
-		reader1.read();
-
-		String path2 = "../datasets/dataset2.txt";
-		CSVReader reader2 = new CSVReader(path2);
-		reader2.read();
-
-
-		String path3 = "../datasets/dataset3.txt";
-		CSVReader reader3 = new CSVReader(path3);
-		reader3.read();*/
-
-		/*
-		Grafo<Integer> g1= reader1.getInfo() ;
-		Grafo<Integer> g2= reader2.getInfo() ;
-		Grafo<Integer> g3= reader3.getInfo() ;
-		*/
-
-		/*
-		ServGreedy greedy= new ServGreedy(g);
-		System.out.println(greedy.dijkstra());
-		System.out.println(greedy.getDistancia());
-		*/
-
-		/*
-		ServBacktracking back= new ServBacktracking(g);
-		System.out.println(back.caminos());
-		System.out.println(back.distanciaTotal);
-		*/
-
-		/*
-		ServicioBack back= new ServicioBack(g);
-		System.out.println(back.caminos());
-		System.out.println(back.distanciaTotal);
-		*/
-		
-
-		
 	}
 
 
